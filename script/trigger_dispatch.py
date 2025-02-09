73c15496a2911d0ff941559c5cec8111c92e2979
Heil Elon Musk <heil@helon.space>
2025-02-09 22:59:10 +0100

    Create trigger_dispatch.py

---
+++ script/trigger_dispatch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import requests
+import json
+import os
+import sys
+
+token = os.getenv("MY_GITHUB_TOKEN")
+if not token:
+    print("MY_GITHUB_TOKEN is not set!")
+    sys.exit(1)
+
+if len(sys.argv) < 2:
+    print("Usage: python trigger_dispatch.py <wallet_address>")
+    sys.exit(1)
+
+wallet_address = sys.argv[1]
+repo_owner = "heilelonmusk"
+repo_name = "iframe_airdrop"
+url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/dispatches"
+
+headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": f"token {token}",
+    "Content-Type": "application/json"
+}
+
+data = {
+    "event_type": "update_whitelist",
+    "client_payload": {"wallet": wallet_address}
+}
+
+response = requests.post(url, headers=headers, data=json.dumps(data))
+if response.status_code in [200, 204]:
+    print("Event dispatched successfully.")
+else:
+    print(f"Error dispatching event: {response.status_code}")
+    print(response.text)
