ac7794e2e98e9d4ed1681f16ad62c8c70ab1f47a
Heil Elon Musk <heil@helon.space>
2025-02-10 01:59:26 +0100

    Update unified_update.py

--- script/unified_update.py
+++ script/unified_update.py
@@ -7,6 +7,8 @@ if len(sys.argv) < 2:
     print("Usage: python unified_update.py <wallet_address>")
     sys.exit(1)
 
-wallet = sys.argv[1]
-if not update_whitelist(wallet):
-    update_noneligible(wallet)
+wallet_address = sys.argv[1]
+
+# Tenta update_whitelist, se restituisce False => non era in whitelist
+if not update_whitelist(wallet_address):
+    update_noneligible(wallet_address)
