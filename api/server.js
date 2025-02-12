23133992e288afc476929f37a779d905a4ce7333
Heil Elon Musk <heil@helon.space>
2025-02-12 06:55:55 +0100

    ciao

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ if (!MONGO_URI) {
 
 // Middleware globale per CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://helon.space");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") {
