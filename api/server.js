303e3c278c9616d6b41ad18e7ead8641c0b52d14
Heil Elon Musk <heil@helon.space>
2025-02-12 08:24:07 +0100

    c

--- api/server.js
+++ api/server.js
@@ -27,10 +27,8 @@ if (!MONGO_URI) {
 app.use(express.json());
 
 // Connessione a MongoDB
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(MONGO_URI)
+
   .then(() => console.log("✅ Connected to MongoDB Atlas"))
   .catch(err => {
     console.error("❌ MongoDB connection error:", err);
