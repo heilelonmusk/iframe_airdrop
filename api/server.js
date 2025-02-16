af36c8d00dfd71699c780e6182328af59db9c79c
Auto Commit <auto@local.com>
2025-02-16 08:46:35 +0100

    vfvrfvref

--- api/server.js
+++ api/server.js
@@ -110,8 +110,6 @@ const connectMongoDB = async () => {
   // Proviamo a connetterci
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 50000,
       socketTimeoutMS: 60000,
       connectTimeoutMS: 60000
