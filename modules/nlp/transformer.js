1934c77f7d7c3659de4bed21482f357c0608ba9e
Auto Commit <auto@local.com>
2025-02-16 18:58:17 +0100

    fvfvfvfvfv

--- modules/nlp/transformer.js
+++ modules/nlp/transformer.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const OpenAI = require("openai");
 const mongoose = require("mongoose");
-const { logger, logConversation, getFrequentQuestions } = require("../modules/logging/logger");
+const { logger, logConversation, getFrequentQuestions } = require("../logging/logger");
 
 // ✅ Load OpenAI API key
 const openai = new OpenAI({
